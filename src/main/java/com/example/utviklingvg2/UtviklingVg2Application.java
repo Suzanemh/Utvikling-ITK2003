9bf21df5767dcbef7967a1aa3195eef527108485
Suzanemh <91255185+Suzanemh@users.noreply.github.com>
2023-08-23 13:45:36 +0200

    Initial commit

---
+++ src/main/java/com/example/utviklingvg2/UtviklingVg2Application.java
@@ -0,0 +1,13 @@
+package com.example.utviklingvg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtviklingVg2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtviklingVg2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/utviklingvg2/UtviklingVg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.utviklingvg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtviklingVg2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
